3e7901e7ab348b6cb3f67e142c7b30e5b62405c1
tuckerward20 <tuckerward20@gmail.com>
2021-01-26 13:00:58 -0600

    exercise upload

---
+++ SyntaxSugarInterpolationExercise/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace SyntaxSugarInterpolationExercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var answer = 4;
+            string response;
+
+            response = (answer < 9) ? $"{answer} is less than nine" : $"{answer} greater than or equal to nine";
+            Console.WriteLine(response);
+        }
+    }
+}
